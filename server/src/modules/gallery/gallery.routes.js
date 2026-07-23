78d8fffafacad359cb5f2e5ad593f07fa505e2b4
Muskan Sharma <214997419+pyschcutieee@users.noreply.github.com>
2026-07-23 18:18:13 +0530

    Updated frontend pages

--- server/src/modules/gallery/gallery.routes.js
+++ server/src/modules/gallery/gallery.routes.js
@@ -1,6 +1,26 @@
-import express from 'express';
-import { getAll } from './gallery.controller.js';
+import express from "express";
+
+import {
+    getAll,
+    getFolders,
+    getFolderMedia,
+    getMedia,
+} from "./gallery.controller.js";
 
 const router = express.Router();
-router.get('/', getAll);
+
+router.get("/", getAll);
+
+router.get("/folders", getFolders);
+
+router.get(
+    "/folders/:folderId/media",
+    getFolderMedia
+);
+
+router.get(
+    "/media/:mediaId",
+    getMedia
+);
+
 export default router;
